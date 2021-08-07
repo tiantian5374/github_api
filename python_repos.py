bf2130227ff16596e5b99c77598b51ead7bd332c
tiantian5374 <tiantian5374@163.com>
2021-08-07 21:37:20 +0800

    查看仓库的基本信息

--- python_repos.py
+++ python_repos.py
@@ -8,6 +8,14 @@ r = requests.get(url, headers=headers)
 print(f'Status code: {r.status_code}')
 # 将API响应赋给一个变量
 response_dict = r.json()
+print(f'Total repositories: {response_dict["total_count"]}')
 
-# 处理结果
-print(response_dict.keys())
+# 搜索有关仓库的信息
+repo_dicts = response_dict['items']
+print(f"Repositories returned: {len(repo_dicts)}")
+
+# 研究第一个仓库
+repo_dict = repo_dicts[0]
+print(f"\nKeys: {len(repo_dict)}")
+for key in sorted(repo_dict.keys()):
+    print(key)
